d1c102d0ac769277ad58599737292993241cf10b
Anuroop <anuroopjoy@gmail.com>
2022-05-03 15:11:06 +0530

    style

--- src/app/table-row/table-row.component.ts
+++ src/app/table-row/table-row.component.ts
@@ -1,7 +1,7 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-table-row',
+  selector: 'tr[app-table-row]',
   templateUrl: './table-row.component.html',
   styleUrls: ['./table-row.component.scss'],
 })
